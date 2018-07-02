fe72d2dfd72bf1a8a9103f9932a7a25b58f62d6a
PatrykPodworski <patrykpodworski@gmail.com>
2018-07-03 01:45:19 +0200

    Scrapy sharp link extration example

--- Scraper/Crawlers/OlxServiceCrawler.cs
+++ Scraper/Crawlers/OlxServiceCrawler.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using OfferLinkScraper.Repositories;
+using ScrapySharp.Network;
+using System;
+using ScrapySharp.Extensions;
 
 namespace OfferLinkScraper.Crawlers
 {
@@ -9,16 +12,16 @@ namespace OfferLinkScraper.Crawlers
         public int PageCounter { get; set; }
 
         protected override string BaseUri => "https://www.olx.pl/gdansk/q-mieszkanie/";
-        protected override string AdvertisementClassName => "marginright5 link linkWithHash";
+        protected override string AdvertisementClassName => "a.marginright5.link.linkWithHash";
 
         public override IEnumerable<Link> GetLinks()
         {
+            var browser = new ScrapingBrowser();
+            var page = browser.NavigateToPage(new Uri(BaseUri));
+            var aTags = page.Html.CssSelect(AdvertisementClassName);
             LinkCounter = LinkLocalFileRepository.GetMaxId();
-            var htmlDocument = GetHtmlDocFromUri();
-            var adLinks = htmlDocument.DocumentNode.Descendants().Where(x => x.GetAttributeValue("class", "").Contains(AdvertisementClassName))
-                .Select(x => x.GetAttributeValue("href", "")).Distinct().Select(x => new Link((++LinkCounter).ToString(), x));
-
-            return adLinks;
+            var links = aTags.Select(x => new Link((++LinkCounter).ToString(), x.Attributes["href"].Value));
+            return links;
         }
     }
 }
