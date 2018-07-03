3b4868e43b6b5217e6d60a13f4001190fb6a7f91
Filip Porzecki <Filip.Porzecki@ihs.com>
2018-07-03 11:19:01 +0200

    add depagination for otodom ads

--- Scraper/Crawlers/OtodomServiceCrawler.cs
+++ Scraper/Crawlers/OtodomServiceCrawler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using OfferLinkScraper.Repositories;
+using ScrapySharp.Extensions;
 using ScrapySharp.Network;
 
 namespace OfferLinkScraper.Crawlers
@@ -9,18 +10,36 @@ namespace OfferLinkScraper.Crawlers
     public class OtodomServiceCrawler : ServiceCrawler
     {
         private static string BaseUri => "https://www.otodom.pl/wynajem/mieszkanie/gdansk/?search%5Bdist%5D=0&search%5Bsubregion_id%5D=439&search%5Bcity_id%5D=40";
+        private static string PageNumberBlockClassName => "pager-counter";
+        private static string PageQuery => $"&page=";
         private static string AdvertisementClassName => "listing_no_promo";
 
         public override IEnumerable<Link> GetLinks()
         {
+            var links = new List<Link>();
             LinkCounter = LinkCounter == 1 ? LinkLocalFileRepository.GetMaxId() : LinkCounter;
             var browser = new ScrapingBrowser();
-            var page = browser.NavigateToPage(new Uri(BaseUri));
-            var aTags = page.Html.Descendants().Where(x =>
-                x.GetAttributeValue("data-featured-tracking", "").Contains(AdvertisementClassName)).ToList();
-            var links = aTags.Select(x => x.GetAttributeValue("href", "")).Distinct().Select(x => new Link((++LinkCounter).ToString(), x));
+            var pagesCount = GetPagesCount(browser);
+
+            for (var i = 1; i <= pagesCount; i++)
+            {
+                var pageQuery = i > 1 ? $"{PageQuery}{i}" : string.Empty;
+                var page = browser.NavigateToPage(new Uri($"{BaseUri}{pageQuery}"));
+                var aTags = page.Html.Descendants().Where(x =>
+                    x.GetAttributeValue("data-featured-tracking", "").Contains(AdvertisementClassName)).ToList();
+                links.AddRange(aTags.Select(x => x.GetAttributeValue("href", "")).Distinct().Select(x => new Link((++LinkCounter).ToString(), x)));
+            }
 
             return links;
+        }
+
+        private static int GetPagesCount(ScrapingBrowser browser)
+        {
+            var page = browser.NavigateToPage(new Uri($"{BaseUri}"));
+            return int.Parse(page.Html.Descendants().Where(x =>
+                    x.GetAttributeValue("class", "").Contains(PageNumberBlockClassName))
+                .SelectMany(x => x.ChildNodes.Where(y => y.Name == "strong")).Last()
+                .InnerText);
         }
     }
 }
