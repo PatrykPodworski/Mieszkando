c886c1ee55946000785bd9a65cbf87867f6f6e3a
Rzecki <filip.porzecki@gmail.com>
2018-08-04 17:05:52 +0200

    added limited quantity queries to repositories

---
+++ MarklogicDataLayer/XQuery/Functions/FnSubsequence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarklogicDataLayer.XQuery.Functions
+{
+    public class FnSubsequence : Function
+    {
+        private readonly Expression _expression;
+        private readonly long _numberOfDocuments;
+
+        public FnSubsequence(Expression expression, long numberOfDocuments = long.MinValue)
+        {
+            _expression = expression;
+            _numberOfDocuments = numberOfDocuments;
+        }
+
+        public override string Query => _numberOfDocuments == long.MinValue
+            ? FunctionToQuery("fn:subsequence", _expression.Query, "1")
+            : FunctionToQuery("fn:subsequence", _expression.Query, "1", _numberOfDocuments.ToString());
+    }
+}
