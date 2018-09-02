8105cfd2d568722b61e803ad1249b5a774b5d7ec
Rzecki <filip.porzecki@gmail.com>
2018-09-02 21:42:41 +0200

    locator uri builder first prototype

---
+++ WebAPI/Utils/LocatorUriBuilder.cs
@@ -0,0 +1,40 @@
+﻿using MarklogicDataLayer.DataStructs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Utils
+{
+    public static class LocatorUriBuilder
+    {
+        private const string _apiKey = "&key=AIzaSyCOSgW_ndo0wm22a1Ncgv260jWgaW2cfnQ";
+        private const string _unit = "metric";
+        private const string _start = "&origins=";
+        private const string _end = "&destinations=";
+        private static readonly string _url = $"https://maps.googleapis.com/maps/api/distancematrix/json?units={_unit}";
+
+        public static List<string> BuildUris(IEnumerable<Offer> offers, IEnumerable<PointOfInterest> pois)
+        {
+            //TODO: think of better exception messages
+            if (offers.Count() < 1)
+            {
+                throw new ArgumentException("Invalid offer amount");
+            }
+            if (pois.Count() < 1)
+            {
+                throw new ArgumentException("Specify at least 1 point of interest");
+            }
+
+            var result = new List<string>();
+
+            foreach (var offer in offers)
+            {
+                var poisString = string.Join('|', pois.Select(x => $"{x.Name},{x.Address}"));
+                result.Add($"{_url}{_start}{offer.Area}{_end}{poisString}{_apiKey}");
+            }
+
+            return result;
+        }
+    }
+}
