55064380e273092744600ecf2e2fc617a151b77f
PatrykPodworski <patrykpodworski@gmail.com>
2018-11-03 13:41:44 +0100

    Style slider

--- web/src/singleSlider/singleSlider.js
+++ web/src/singleSlider/singleSlider.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Nouislider from "nouislider-react";
 import "nouislider/distribute/nouislider.css";
+import "./singleSlider.css";
+import wNumb from "wnumb";
 
 export default class SingleSlider extends Component {
     render(){
@@ -8,8 +10,19 @@ export default class SingleSlider extends Component {
             <Nouislider 
             range={{ min: this.props.min, max: this.props.max }} 
             start={[this.props.start]} 
-            connect 
-            tooltips/>
+            step={100}
+            connect={[true, false]}
+            tooltips={true}
+            format={wNumb({
+                decimals: 0,
+                suffix: ' zł'
+            })}
+            />
         );
+    }
+
+    formatNumber(number) {
+        number = 'qwe';
+        return true;
     }
 }
