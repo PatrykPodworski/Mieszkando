ca3c1081dcc290d531c99b05485b6f8e59c823fb
PatrykPodworski <patrykpodworski@gmail.com>
2018-11-29 18:41:52 +0100

    Add key to list

--- web/src/listOfOffers/listOfOffers.js
+++ web/src/listOfOffers/listOfOffers.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import OfferGroup from '../offerGroup/offerGroup';
 
-export default class listOfOffers extends Component {
+export default class ListOfOffers extends Component {
   render() {
     return (
       <div>
           {this.props.offers
             .sort((x,y) => y.offers.length - x.offers.length )
-            .map((group) => {
+            .map((group, i) => {
             return (
-              <OfferGroup group={group}/>)
+              <OfferGroup group={group} key={i}/>)
           })}
       </div>
     )
